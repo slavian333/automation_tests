d23ef51c4e3488e68dfb3c148f47209fd0ff87d0
Yevhen Petryk <yevhen.petryk@cytiva.com>
2024-12-06 16:18:02 +0200

    Fix dependencies and tests

--- tests/addToCart.spec.js
+++ tests/addToCart.spec.js
@@ -1,20 +1,22 @@
 describe('Login', () => {
-    it('fill in valid data and login', async () => {
-        await browser.url(`https://www.saucedemo.com/`);
+  it('fill in valid data and login', async () => {
+    await browser.url(`https://www.saucedemo.com/`);
 
-        await $('#user-name').setValue('standard_user');
-        await $('#password').setValue('secret_sauce');
-        await $('#login-button').click();
-        
-        await $('#add-to-cart-sauce-labs-backpack').click();
-        await $('//div[@id="shopping_cart_container"]//span[text()="1"]').toBeExisting();
-        // await $('div .title').toBeExisting();
-        await $('#shopping_cart_container').click();
+    await $('#user-name').setValue('standard_user');
+    await $('#password').setValue('secret_sauce');
+    await $('#login-button').click();
 
-        const cartItem = await $('#item_4_title_link');
-        await cartItem.toBeExisting();
-        await cartItem.click();
+    await $('#add-to-cart-sauce-labs-backpack').click();
+    await $(
+      '//div[@id="shopping_cart_container"]//span[text()="1"]',
+    ).toBeExisting();
+    // await $('div .title').toBeExisting();
+    await $('#shopping_cart_container').click();
 
-        // await $('.inventory_item:nth-child(2)').toBeExisting();
-    })
-})
+    const cartItem = await $('#item_4_title_link');
+    await cartItem.toBeExisting();
+    await cartItem.click();
+
+    // await $('.inventory_item:nth-child(2)').toBeExisting();
+  });
+});
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,15 +1,13 @@
-import { describe, it } from 'mocha';
- 
 describe('Login', () => {
-    it('fill in valid data and login', async () => {
-        await browser.url(`https://www.saucedemo.com/`);
+  it('fill in valid data and login', async () => {
+    await browser.url(`https://www.saucedemo.com/`);
 
-        await $('#user-name').setValue('standard_user');
-        await $('#password').setValue('secret_sauce');
-        await $('#login-button').click();
-        
-        await $('div .title').toBeExisting();
-        await $('#shopping_cart_container').toBeExisting();
-        await $('.inventory_item:nth-child(2)').toBeExisting();
-    })
-})
+    await $('#user-name').setValue('standard_user');
+    await $('#password').setValue('secret_sauce');
+    await $('#login-button').click();
+
+    await $('div .title').toBeExisting();
+    await $('#shopping_cart_container').toBeExisting();
+    await $('.inventory_item:nth-child(2)').toBeExisting();
+  });
+});
